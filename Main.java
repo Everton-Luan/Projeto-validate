de4a2d1bb76501b3db955ff48c14e8f64d233e46
Everton <everton.luan@ufrpe.br>
2026-04-28 09:28:05 -0300

    :sparkles: Feat: Adicionando verificador de telefone/celular

--- Main.java
+++ Main.java
@@ -6,9 +6,16 @@ public class Main {
 
         System.out.println("Digite o seu cpf: ");
         String cpf = sc.nextLine();
+        System.out.println("Digite o seu telefone/celular: ");
+        String cell = sc.nextLine();
 
-        VerificadorCPF verificar = new VerificadorCPF();
-        boolean status = verificar.validarCPF(cpf);
-        System.out.println(status);
+        VerificadorCPF vCPF = new VerificadorCPF();
+        VerificadorCell vCell = new VerificadorCell();
+
+        boolean statusCPF = vCPF.validarCPF(cpf);
+        boolean statusCell = vCell.validarCell(cell);
+
+        System.out.println(statusCPF);
+        System.out.println(statusCell);
     }
 }
